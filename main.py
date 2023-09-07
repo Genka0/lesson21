394a9998c3ab93bfe2853c4a5a446cd314eeb000
Nesterenko Iryna <irane100r@gmail.com>
2023-09-07 17:47:56 +0200

    message answering

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types, executor
 
 from decouple import config
 
 bot = Bot(config('API_TOKEN'))
 dp = Dispatcher(bot)
 # @ - декоратор. фукция, которая содержит в себе нашу функцию
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привіт, я допоможу тобі порахувати А + Б")
+    await message.answer("Привіт, я бот, що зможе відповісти на кілька твоїх запитань!")
+@dp.message_handler(commands = ["help"])
+async def command_help(message: types.Message):
+    await message.answer("Напиши мені 2+2")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # await message.answer(message.text)
+    print(message)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True)
