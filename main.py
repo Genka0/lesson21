d431c5239c72ede5d9f67a77f912202915ff0d6a
Nesterenko Iryna <irane100r@gmail.com>
2023-09-07 18:22:44 +0200

    changes

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from aiogram import Bot, Dispatcher, types, executor
+from decouple import Config, Csv
 
-from decouple import config
+config = Config()
+config.read(".env")
 
-bot = Bot(config('API_TOKEN'))
+bot = Bot(config("API_TOKEN"))
 dp = Dispatcher(bot)
-# @ - декоратор. фукция, которая содержит в себе нашу функцию
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.answer("Привіт, я бот, що зможе відповісти на кілька твоїх запитань!")
-@dp.message_handler(commands = ["help"])
+    await message.reply("Привіт, я бот, що зможе відповісти на кілька твоїх запитань!")
+
+@dp.message_handler(commands=["help"])
 async def command_help(message: types.Message):
     await message.answer("Напиши мені 2+2")
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    # await message.answer(message.text)
-    print(message)
-
+    await message.answer(message.text)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates = True)
+    executor.start_polling(dp)
